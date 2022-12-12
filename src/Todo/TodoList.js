f07e2ab45bc30160d5cb3f7c42610d1e396e1b23
Torishnia <valeri.torishnya@gmail.com>
2022-12-12 18:35:05 +0300

    add prop-types

--- src/Todo/TodoList.js
+++ src/Todo/TodoList.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import TodoItem from './TodoItem';
 
-export default function TodoList(props) {
+function TodoList(props) {
     return (
         <ul>
-           {props.todos.map(todo => {
-              return <TodoItem todo={todo} key={todo.id}/>
+           {props.todos.map((todo, index) => {
+              return <TodoItem todo={todo} key={todo.id} index={index}/>
            })}
         </ul>
     )
 }
+
+TodoList.propTypes = {
+    todos: PropTypes.arrayOf(PropTypes.object).isRequired
+}
+
+export default TodoList
