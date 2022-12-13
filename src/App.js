4838fd30803ec8ce442c8ddd6b771c71299af002
Torishnia <valeri.torishnya@gmail.com>
2022-12-13 14:26:58 +0300

    create loader

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoList from './Todo/TodoList';
 import Context from './context';
 import AddTodo from './Todo/AddTodo';
+import Loader from './Loader';
 
 function App() {
   const [todos, setTodos] = React.useState([
-    {id: 1, completed: false, title: 'Buy bread'},
+    /*{id: 1, completed: false, title: 'Buy bread'},
     {id: 2, completed: false, title: 'Buy milk'},
-    {id: 3, completed: false, title: 'Buy oil'},
-  ])
+    {id: 3, completed: false, title: 'Buy oil'},*/
+  ]);
 
- 
+  const [loading, setLoading] = React.useState(true);
+
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
+    .then(response => response.json())
+    .then(todos => {
+      setTimeout(() => {
+        setTodos(todos);
+        setLoading(false);
+      },2000)
+    })
+},[])
 
   function toggleTodo(id) {
     setTodos(
@@ -42,9 +54,10 @@ function App() {
       <div className="wrapper">
       <h1>React tutorial</h1>
       <AddTodo onCreate={addTodo}/>
+      {loading && <Loader/>}
       {todos.length ? (
       <TodoList todos={todos} onToggle={toggleTodo}/>
-      ) : (
+      ) : loading ? null : (
       <p>No todos!</p> 
       )}  
    </div>
